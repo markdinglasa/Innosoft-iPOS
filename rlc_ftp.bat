67df7c6019e063d21d9f2caaab270c23e3fe161b
Mark Dinglasa <ryanmark.dinglasa@gmail.com>
2024-08-31 09:43:45 +0800

    updated database to v2022

--- rlc_ftp.bat
+++ rlc_ftp.bat
@@ -1,18 +1,17 @@
 @echo off
-REM Source folder location
+if "%1"=="min" goto :minimized
+
+start /min cmd /c "%~f0" min & exit
+
+:minimized
 set source="C:\Users\ryanm\Desktop\ROBINSONS\RLC"
 
-REM Destination folder on the target computer
-set destination="\\192.168.1.132\Users\Public\Shared-POS"
+set destination="\\192.168.1.99\entoi"
 
-REM Transferring the entire folder
 xcopy %source% %destination% /E /I /Y
 
-REM Checking if the transfer was successful
 if %errorlevel%==0 (
     echo Folder transferred successfully!
 ) else (
     echo Folder transfer failed.
 )
-
-pause
