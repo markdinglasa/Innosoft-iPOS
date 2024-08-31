b025eaf6c9cc9b5b512e73672cfeffe852d125b0
Mark Dinglasa <ryanmark.dinglasa@gmail.com>
2024-08-31 16:53:53 +0800

    update RLC to MYK

--- rlc_ftp.bat
+++ rlc_ftp.bat
@@ -6,7 +6,7 @@ start /min cmd /c "%~f0" min & exit
 :minimized
 set source="C:\Users\ryanm\Desktop\ROBINSONS\RLC"
 
-set destination="\\192.168.1.99\entoi"
+set destination="\\192.168.1.115\shared-folder\RLC"
 
 xcopy %source% %destination% /E /I /Y
 
