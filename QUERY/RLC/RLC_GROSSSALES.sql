09fdb6f9b5098e8d23d0e37dfe9ab6a532ee1d52
Mark Dinglasa <ryanmark.dinglasa@gmail.com>
2024-09-21 14:56:42 +0800

    updated and fix RLC

--- QUERY/RLC/RLC_GROSSSALES.sql
+++ QUERY/RLC/RLC_GROSSSALES.sql
@@ -1,7 +1,7 @@
 SELECT 
 DFirst("RLC_TenantId","SysCurrent") AS TenantId, 
 [TrnSales].[TerminalId] AS TerminalId,
-Sum(Round(IIf([MstDiscount].[Discount]<>"Senior Citizen Discount" And [MstDiscount].[Discount]<>"PWD" AND (Nz([TrnCollection].[IsReturn], 0) <> 2),[Price],[Price1]+[Price2LessTax])*[Quantity],3)) AS GrossSales, 
+Sum(Round(IIf([MstDiscount].[Discount]<>"Senior Citizen Discount" And [MstDiscount].[Discount]<>"PWD" AND (Nz([TrnCollection].[IsReturn], 0) <> 2),[Price],[Price1]+[Price2LessTax])    *[Quantity],3)) AS GrossSales, 
 Sum(IIf(([TrnSalesLine].[TaxRate] > 0) AND (Nz([TrnCollection].[IsReturn], 0) < 1), [TrnSalesLine].[TaxAmount], 0)) AS TaxAmount,
 Sum(IIF(Nz([TrnCollection].[IsReturn], 0) = 2, 0, [TrnSalesLine].[Amount])) AS NetSales
 INTO [RLC_GROSSSALES]
@@ -13,6 +13,7 @@ FROM TrnSales
 WHERE 
     [TrnSales].[IsLocked] = True 
     AND [TrnCollection].[IsLocked] = True 
+    AND TrnSales.IsCancelled = False AND TrnCollection.IsCancelled = False  
     AND DAY([TrnSales].[SalesDate]) = DAY([Forms]![SysSettings]![RLC_DateMem]) 
     AND MONTH([TrnSales].[SalesDate]) = MONTH([Forms]![SysSettings]![RLC_DateMem]) 
     AND YEAR([TrnSales].[SalesDate]) = YEAR([Forms]![SysSettings]![RLC_DateMem]) 
